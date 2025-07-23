84c3bf240a3dddf08820b115d06624fadca6ef68
Christina <peacechristina32@gmail.com>
2025-07-23 23:42:20 +0500

    Add navbar in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
 import { blogs } from '../data/blogs';
 import BlogCard from '../components/BlogCard';
+import Navbar from '@/components/Navbar';
 
 export default function HomePage() {
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-6">Latest Blog Posts</h1>
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {blogs.map(blog => (
-          <BlogCard key={blog.id} blog={blog} />
-        ))}
+    <>
+      <Navbar />
+      <div className="p-6">
+        <h1 className="text-3xl font-bold mb-6">Latest Blog Posts</h1>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {blogs.map(blog => (
+            <BlogCard key={blog.id} blog={blog} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
